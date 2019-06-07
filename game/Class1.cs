1ed74461048378f1f9532abb96c139f0a15314a2
Masahiro Yoshida <mypianoplayer@gmail.com>
2019-06-07 18:20:49 +0900

    cs recv-send

--- game/Class1.cs
+++ game/Class1.cs
@@ -2,6 +2,15 @@
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Text;
+// using Lockfreeq;
 
 namespace game
 {
@@ -26,15 +35,80 @@ namespace game
             Console.WriteLine("{0}", System.Reflection.Assembly.GetExecutingAssembly().FullName);
             Console.WriteLine("This assembly is not meant to be run directly.");
             Console.WriteLine("Instead, please use the SampleHost process to load this assembly.");
-            var sw = new System.Diagnostics.Stopwatch();
-            sw.Start();
-            for(int i = 0;i < 1000000;i++) {
-                OnReceiveCS(30,40);
-            }
-            sw.Stop();
-            TimeSpan ts = sw.Elapsed;
-            Console.WriteLine("time {0}", (int)ts.TotalMilliseconds);
+
+            StartServer();
+            Thread.Sleep(1000000);
+
+            // var sw = new System.Diagnostics.Stopwatch();
+            // sw.Start();
+            // for(int i = 0;i < 1000000;i++) {
+            //     OnReceiveCS(30,40);
+            // }
+            // sw.Stop();
+            // TimeSpan ts = sw.Elapsed;
+            // Console.WriteLine("time {0}", (int)ts.TotalMilliseconds);
         }
+
+        static async void StartServer()
+        {
+            TcpListener listener = new TcpListener(29180);
+            listener.Start();
+            // List<Channel> room_channels = new List<Channel>();
+            while (true) {
+                // 2. クライアントからの接続を受け入れる
+                var socket = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("acc");
+
+                Task.Run( async()=>{
+                    var writer = new StreamWriter(socket.GetStream(), System.Text.Encoding.ASCII);
+                    var reader = new StreamReader(socket.GetStream(), System.Text.Encoding.ASCII);
+
+                    while(true) {
+                        var line = await reader.ReadLineAsync();
+
+                        if(line.StartsWith("input_text")) {
+                            ObjInfo[] data = new ObjInfo[50];
+                            for(int i = 0; i < 50; i++) {
+                                data[i].Name = "aaaaa";
+                                data[i].X = 1;
+                                data[i].Y = 2;
+                            }
+
+                            StringBuilder sb = new StringBuilder();
+                            foreach( var d in data ) {
+                                sb.AppendFormat("{0}:{1},{2}/", "testname", 1, 2);
+                            }
+                            // Console.WriteLine("msg {0}", line);
+                            await writer.WriteLineAsync(sb.ToString());
+                            await writer.FlushAsync();
+                        }
+                    }
+                        // var data = await socket.ReceiveAsync(SocketAsyncEventArgs.Empty);
+                });
+
+
+
+
+                // BufferBlock<TcpClient> channel = null;
+                // if(room_channels.Count == 0) {
+                //     channel = NewRoom();
+                //     room_channels.Add(new Channel(channel,1));
+                //     Console.WriteLine("new room {0}", room_channels.Count);
+                // }
+                // else if(room_channels[room_channels.Count-1].member_count < 10) {
+                //     channel = room_channels[room_channels.Count-1].channel;
+                //     room_channels[room_channels.Count-1].member_count++;
+                //     Console.WriteLine("new member {0}", room_channels[room_channels.Count-1].member_count);
+                // }
+                // else {
+                //     channel = NewRoom();
+                //     room_channels.Add(new Channel(channel,1));
+                //     Console.WriteLine("new room {0}", room_channels.Count);
+                // }
+                // await channel.SendAsync(client);
+            }
+        }
+
 
         public delegate void SendFn([In,Out] ObjInfo[] objinfo, int size);
 
